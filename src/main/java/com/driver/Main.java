2eaa84c8fbac838c41c972ff1cd6a596520f4f4c
kunal <kunaldayal.786.kd@gmail.com>
2023-01-14 12:59:07 +0530

    1st try

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+    B call = new B();
+    String lol = "see what happens";
+    String see = B.meth(lol);
+    }
+}
+class A {
+    public static String meth (String check ){
+        return check;
+    }
+}
+class B extends A {
+    public static String meth (String check ){
+        return check;
+    }
 }
