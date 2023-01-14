d14bc3afce709808d8aa1928bbafdc2ac122f288
kunal <kunaldayal.786.kd@gmail.com>
2023-01-14 16:28:23 +0530

    3rd try

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,15 +5,15 @@ public class Main {
         B b=new B();
         System.out.println(b.meth());
     }
-}
- class A{
-    public static String meth(){
-        return "Invoking method from class A";
-    }
+    public static class A{
+        public static String meth(){
+            return "Invoking method from class A";
+        }
 
-}
- class B extends A{
-    public static String meth(){
-        return "Method is overridden in Extendend class B";
+    }
+    public static class B extends A{
+        public static String meth(){
+            return "Method is overridden in Extendend class B";
+        }
     }
 }
